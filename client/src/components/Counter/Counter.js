cafce773b1f7d5c4b7b02797d210e7cdefd17410
avivcobenbs@gmail.com <avivcobenbs@gmail.com>
2022-11-03 11:44:55 +0200

    Delete Counter

--- client/src/components/Counter/Counter.js
+++ client/src/components/Counter/Counter.js
@@ -1,17 +1,15 @@
-import React, { useState } from 'react';
-import './Counter.css';
+// import React, { useState } from 'react';
+// import './Counter.css';
 
+// function Counter() {
+//     const [count, setCount] = useState(true);
 
-function Counter() {
-    const [count, setCount] = useState(true);
+//     return (
+//         <div className="App">
+//             <button className="btn" onClick={() => setCount(!count)}>Welcome msg</button>
+//             {count && <div>Welcome to GoCode Shop</div>}
+//         </div>
+//     );
+// }
 
-    return (
-        <div className="App">
-            <button className="btn" onClick={() => setCount(!count)}>Welcome msg</button>
-            {count && <div>Welcome to GoCode Shop</div>}
-        </div>
-    );
-}
-
-
-export default Counter;
+// export default Counter;
