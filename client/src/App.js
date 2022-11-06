d0e15a733a29bc0688869c846d024ebac3a09cef
avivcobenbs@gmail.com <avivcobenbs@gmail.com>
2022-11-06 20:57:44 +0200

    fix error

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Link to="/about">About our shop</Link>
       <br />
       <br />
-      <Counter />
+      {/* <Counter /> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
